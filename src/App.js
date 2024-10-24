1b2835365ed9dac5138bb8826e0e9324443f0af7
MarkFinn0 <marquinhossouzarodrigueslive@gmail.com>
2024-10-24 00:20:10 -0300

    Subindo Alterações

--- src/App.js
+++ src/App.js
@@ -8,21 +8,29 @@ import Offcanvas from 'react-bootstrap/Offcanvas';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { IoMdAddCircle, IoMdEye, IoMdSettings, IoMdDownload } from 'react-icons/io';
 import logo from './logo.png';
+import logo2 from './logo2.png'; // Certifique-se de que o caminho esteja correto
+
 
 function App() {
-  const [height, setHeight] = useState('');
+
+  const [showExitPresentation, setShowExitPresentation] = useState(false);
+
   const [url, setUrl] = useState('');
   const [name, setName] = useState('');
+  const [height, setHeight] = useState('');
+  const [scrollInterval, setScrollInterval] = useState(1); // em segundos
+  const [scrollAmount, setScrollAmount] = useState(100); 
+  const [refreshInterval, setRefreshInterval] = useState(30); // em segundos
   const [iframes, setIframes] = useState([]);
   const [showOffCanvas, setShowOffCanvas] = useState(false);
   const [showModal, setShowModal] = useState(false);
   const [presentationMode, setPresentationMode] = useState(false);
-  const [showExitPresentation, setShowExitPresentation] = useState(false);
   const [scrollingDown, setScrollingDown] = useState(true);
-  const [scrollInterval, setScrollInterval] = useState(null);
 
   const containerRef = useRef(null);
   const presentationRef = useRef(null);
+  const scrollTimers = useRef([]);
+  const refreshTimers = useRef([]);
 
   const handleCloseModal = () => setShowModal(false);
   const handleShowModal = () => setShowModal(true);
@@ -30,10 +38,20 @@ function App() {
   const handleSubmit = (e) => {
     e.preventDefault();
     if (url && height) {
-      setIframes([...iframes, { url, height, name }]);
+      setIframes([...iframes, {
+        url,
+        name,
+        height,
+        scrollInterval: scrollInterval * 1000, // Convertendo para ms
+        scrollAmount: scrollAmount,
+        refreshInterval: refreshInterval * 1000, // Convertendo para ms
+      }]);
       setUrl('');
-      setHeight('');
       setName('');
+      setHeight('');
+      setScrollInterval(1); // resetando para 1 segundo
+      setScrollAmount(100);
+      setRefreshInterval(30); // resetando para 30 segundos
       handleCloseModal();
     }
   };
@@ -42,33 +60,88 @@ function App() {
     setIframes(iframes.filter((_, i) => i !== index));
   };
 
+  const startAutoScroll = (index) => {
+    stopAutoScroll(index); // Para qualquer rolagem anterior
+
+    const interval = setInterval(() => {
+      if (presentationRef.current) {
+        const maxScrollTop = presentationRef.current.scrollHeight - presentationRef.current.clientHeight;
+        const currentScrollTop = presentationRef.current.scrollTop;
+
+        if (scrollingDown) {
+          if (currentScrollTop < maxScrollTop) {
+            presentationRef.current.scrollBy({ top: iframes[index].scrollAmount, behavior: 'smooth' });
+          } else {
+            setScrollingDown(false);
+          }
+        } else {
+          if (currentScrollTop > 0) {
+            presentationRef.current.scrollBy({ top: -iframes[index].scrollAmount, behavior: 'smooth' });
+          } else {
+            setScrollingDown(true);
+          }
+        }
+      }
+    }, iframes[index].scrollInterval);
+    scrollTimers.current[index] = interval;
+  };
+
+  const stopAutoScroll = (index) => {
+    if (scrollTimers.current[index]) {
+      clearInterval(scrollTimers.current[index]);
+      scrollTimers.current[index] = null;
+    }
+  };
+
+  const startAutoRefresh = (index) => {
+    stopAutoRefresh(index); // Para qualquer atualização anterior
+  
+    const interval = setInterval(() => {
+      const updatedIframes = [...iframes];
+      // Força a atualização adicionando um timestamp à URL
+      const currentUrl = new URL(iframes[index].url);
+      currentUrl.searchParams.set('t', Date.now()); // Adiciona um timestamp como parâmetro
+      updatedIframes[index].url = currentUrl.toString(); // Atualiza a URL com o timestamp
+      setIframes(updatedIframes); // Atualiza o estado
+    }, iframes[index].refreshInterval);
+    refreshTimers.current[index] = interval;
+  };
+  
+
+
+  const stopAutoRefresh = (index) => {
+    if (refreshTimers.current[index]) {
+      clearInterval(refreshTimers.current[index]);
+      refreshTimers.current[index] = null;
+    }
+  };
+
   const handleEnterPresentationMode = () => {
     setPresentationMode(true);
-    setShowExitPresentation(true);
     containerRef.current.requestFullscreen(); // Entra em tela cheia
-    containerRef.current.style.padding = '0';
-    startAutoScroll();
+    iframes.forEach((_, index) => {
+      startAutoScroll(index);
+      startAutoRefresh(index);
+    });
   };
 
   const handleExitPresentationMode = () => {
     setPresentationMode(false);
-    setShowExitPresentation(false);
     document.exitFullscreen(); // Sai da tela cheia
-    containerRef.current.style.padding = '20px';
-    stopAutoScroll();
+    iframes.forEach((_, index) => {
+      stopAutoScroll(index);
+      stopAutoRefresh(index);
+    });
   };
 
-  const handleMouseMove = () => {
-    if (presentationMode) {
-      setShowExitPresentation(true);
-    }
-  };
-
-  const handleMouseLeave = () => {
-    if (presentationMode) {
-      setShowExitPresentation(false);
-    }
-  };
+  useEffect(() => {
+    return () => {
+      iframes.forEach((_, index) => {
+        stopAutoScroll(index);
+        stopAutoRefresh(index);
+      });
+    };
+  }, [iframes]);
 
   const handleDragStart = (e, index) => {
     e.dataTransfer.setData('index', index.toString());
@@ -86,7 +159,33 @@ function App() {
     updatedFrames.splice(targetIndex, 0, draggedItem);
     setIframes(updatedFrames);
   };
+  const applyStylesToIframeContent = (iframeElement) => {
+    // Removido o código que acessa o documento do iframe, pois isso causará um erro se o iframe for cross-origin.
+    iframeElement.addEventListener("load", () => {
+      // Verifique se o iframe está na mesma origem antes de tentar aplicar estilos.
+      try {
+        const iframeDocument = iframeElement.contentDocument || iframeElement.contentWindow.document;
+        const styleElement = iframeDocument.createElement("style");
+        styleElement.textContent = ".logoBar { display: none !important; background-color: #ff0000 !important }";
+        iframeDocument.head.appendChild(styleElement);
+      } catch (error) {
+        console.error("Não foi possível acessar o conteúdo do iframe:", error);
+      }
+    });
+  };
+  
+  
+  const handleMouseMove = () => {
+    if (presentationMode) {
+      setShowExitPresentation(true);
+    }
+  };
 
+  const handleMouseLeave = () => {
+    if (presentationMode) {
+      setShowExitPresentation(false);
+    }
+  };
   const handleImportConfig = (e) => {
     const file = e.target.files[0];
     const reader = new FileReader();
@@ -109,51 +208,6 @@ function App() {
     a.click();
     URL.revokeObjectURL(url);
   };
-
-  const applyStylesToIframeContent = (iframeElement) => {
-    iframeElement.addEventListener("load", () => {
-      const iframeDocument = iframeElement.contentDocument || iframeElement.contentWindow.document;
-      const styleElement = iframeDocument.createElement("style");
-      styleElement.textContent = ".logoBar { display: none !important; background-color: #ff0000 !important }"; // Estilo para esconder .logoBar
-      iframeDocument.head.appendChild(styleElement);
-    });
-  };
-
-  const startAutoScroll = () => {
-    const interval = setInterval(() => {
-      if (presentationRef.current) {
-        const maxScrollTop = presentationRef.current.scrollHeight - presentationRef.current.clientHeight;
-        const currentScrollTop = presentationRef.current.scrollTop;
-
-        if (scrollingDown) {
-          if (currentScrollTop < maxScrollTop) {
-            presentationRef.current.scrollBy({ top: 100, behavior: 'smooth' });
-          } else {
-            setScrollingDown(false);
-          }
-        } else {
-          if (currentScrollTop > 0) {
-            presentationRef.current.scrollBy({ top: -100, behavior: 'smooth' });
-          } else {
-            setScrollingDown(true);
-          }
-        }
-      }
-    }, 1000); // Intervalo de 1 segundo para rolagem
-    setScrollInterval(interval);
-  };
-
-  const stopAutoScroll = () => {
-    if (scrollInterval) {
-      clearInterval(scrollInterval);
-      setScrollInterval(null);
-    }
-  };
-
-  useEffect(() => {
-    return () => stopAutoScroll();
-  }, []);
-
   return (
     <>
       <Navbar style={{ height: '35px', backgroundColor: '#ff6200' }}>
@@ -238,7 +292,42 @@ function App() {
                         }}
                         style={{ maxWidth: '100%'                      }}
                         style={{ maxWidth: '100%' }}
+                      /> <Form.Group controlId={`formScrollInterval-${index}`}>
+                      <Form.Label>Intervalo de Rolagem (segundos):</Form.Label>
+                      <Form.Control
+                        type="number"
+                        value={iframe.scrollInterval / 1000}
+                        onChange={(e) => {
+                          const updatedIframes = [...iframes];
+                          updatedIframes[index].scrollInterval = parseInt(e.target.value) * 1000; // Convertendo para ms
+                          setIframes(updatedIframes);
+                        }}
                       />
+                    </Form.Group>
+                    <Form.Group controlId={`formScrollAmount-${index}`}>
+                      <Form.Label>Quantidade de Rolagem (pixels):</Form.Label>
+                      <Form.Control
+                        type="number"
+                        value={iframe.scrollAmount}
+                        onChange={(e) => {
+                          const updatedIframes = [...iframes];
+                          updatedIframes[index].scrollAmount = parseInt(e.target.value);
+                          setIframes(updatedIframes);
+                        }}
+                      />
+                    </Form.Group>
+                    <Form.Group controlId={`formRefreshInterval-${index}`}>
+                      <Form.Label>Intervalo de Atualização (segundos):</Form.Label>
+                      <Form.Control
+                        type="number"
+                        value={iframe.refreshInterval / 1000}
+                        onChange={(e) => {
+                          const updatedIframes = [...iframes];
+                          updatedIframes[index].refreshInterval = parseInt(e.target.value) * 1000; // Convertendo para ms
+                          setIframes(updatedIframes);
+                        }}
+                      />
+                    </Form.Group>
                     </div>
                     <Button variant="danger" size="sm" onClick={() => handleDeleteItem(index)}>
                       Deletar
@@ -284,6 +373,30 @@ function App() {
                 onChange={(e) => setName(e.target.value)}
               />
             </Form.Group>
+            <Form.Group controlId="formScrollInterval">
+              <Form.Label>Intervalo de Rolagem (segundos):</Form.Label>
+              <Form.Control
+                type="number"
+                value={scrollInterval}
+                onChange={(e) => setScrollInterval(e.target.value)}
+              />
+            </Form.Group>
+            <Form.Group controlId="formScrollAmount">
+              <Form.Label>Quantidade de Rolagem (pixels):</Form.Label>
+              <Form.Control
+                type="number"
+                value={scrollAmount}
+                onChange={(e) => setScrollAmount(e.target.value)}
+              />
+            </Form.Group>
+            <Form.Group controlId="formRefreshInterval">
+              <Form.Label>Intervalo de Atualização (segundos):</Form.Label>
+              <Form.Control
+                type="number"
+                value={refreshInterval}
+                onChange={(e) => setRefreshInterval(e.target.value)}
+              />
+            </Form.Group>
             <Button variant="primary" type="submit" style={{
               marginTop: '10px',
             }}>
@@ -320,50 +433,64 @@ function App() {
           </div>
         ))}
   
-        {presentationMode && (
-          <div
-            ref={presentationRef}
-            className="frame"
-            style={{
-              position: 'fixed',
-              top: 0,
-              left: 0,
-              width: '100%',
-              height: '100vh',
-              zIndex: 1,
-              overflow: 'auto',
-              background: '#414141',
-            }}
-          >
-            {iframes.map((iframe, index) => (
-              <div key={index} style={{ textAlign: 'center' }}>
-                <iframe
-                  src={iframe.url}
-                  width="100%"
-                  height={iframe.height}
-                  style={{ border: 'none' }}
-                  scrolling="no" // Aqui está o atributo para remover o scroll
-                ></iframe>
-              </div>
-            ))}
-            {showExitPresentation && (
-              <Button
-                variant="light"
-                onClick={handleExitPresentationMode}
-                style={{ position: 'fixed', top: '20px', right: '20px' }}
-              >
-                Sair da Apresentação
-              </Button>
-            )}
-            <Button
-              variant="light"
-              onClick={handleExitPresentationMode}
-              style={{ position: 'fixed', bottom: '20px', right: '20px' }}
-            >
-              Parar Rolagem Automática
-            </Button>
-          </div>
-        )}
+  {presentationMode && (
+  <div
+    ref={presentationRef}
+    className="frame"
+    style={{
+      position: 'fixed',
+      top: 0,
+      left: 0,
+      width: '100%',
+      height: '100vh',
+      zIndex: 1,
+      overflow: 'auto',
+      background: '#414141',
+    }}
+  >
+    {iframes.map((iframe, index) => (
+      <div key={index} style={{ textAlign: 'center' }}>
+        <iframe
+          src={iframe.url}
+          width="100%"
+          height={iframe.height}
+          style={{ border: 'none' }}
+          scrolling="no"
+        ></iframe>
+      </div>
+    ))}
+    {showExitPresentation && (
+      <Button
+        variant="light"
+        onClick={handleExitPresentationMode}
+        style={{ position: 'fixed', top: '20px', right: '20px' }}
+      >
+        Sair da Apresentação
+      </Button>
+    )}
+    <Button
+      variant="light"
+      onClick={handleExitPresentationMode}
+      style={{ position: 'fixed', bottom: '20px', right: '20px' }}
+    >
+      Parar Rolagem Automática
+    </Button>
+    
+    {/* Adicionando o logo2 no rodapé direito */}
+    <img
+      src={logo2}
+      alt="Logo 2"
+      style={{
+        position: 'fixed',
+        bottom: '20px',
+        left: '20px',
+        width: '200px', // Ajuste o tamanho conforme necessário
+        zIndex: 2, // Para garantir que o logo fique acima de outros elementos
+      }}
+    />
+  </div>
+)}
+
       </div>
     </>
   );
